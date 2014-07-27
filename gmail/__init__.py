330e63729f3eb6504cbdf9b8a8bfe287948b97e9
lenovo <arunghosh@gmail.com>
2014-07-27 16:15:08 +0530

    intial_01

--- gmail/__init__.py
+++ gmail/__init__.py
@@ -4,13 +4,20 @@ from apiclient.discovery import build
 from oauth2client.file import Storage
 from auth.models import MyUser
 
+
 class Auth:
 
+    # flow = OAuth2WebServerFlow(
+    #         client_id='864339645094-6u7141gbiprre8u1r122e1tjkghk7ev9.apps.googleusercontent.com',
+    #         client_secret='wlX4AGzMC1YIS_aQdptABlJ2',
+    #         scope='https://www.googleapis.com/auth/gmail.readonly email',
+    #         redirect_uri='http://localhost:9000/oauth2callback')
+
     flow = OAuth2WebServerFlow(
-            client_id='864339645094-6u7141gbiprre8u1r122e1tjkghk7ev9.apps.googleusercontent.com',
-            client_secret='wlX4AGzMC1YIS_aQdptABlJ2',
+            client_id='864339645094-rii2aqr2jm20k63hf6jjt2pr7c6qku31.apps.googleusercontent.com',
+            client_secret='nT3ebkSkWSUGdob-yfxNdCGC',
             scope='https://www.googleapis.com/auth/gmail.readonly email',
-            redirect_uri='http://localhost:9000/oauth2callback')
+            redirect_uri='http://quiet-caverns-2087.herokuapp.com/oauth2callback ')
 
     @staticmethod
     def get_auth_url():
