fa97f34eeb79ab4c4eeabcdf683ca9375458403a
lenovo <arunghosh@gmail.com>
2014-08-03 22:14:09 +0530

    settings change and UI 04

--- _main/settings.py
+++ _main/settings.py
@@ -77,7 +77,10 @@ DATABASES['default'] = dj_database_url.config()
 
 TEMPLATE_CONTEXT_PROCESSORS = ('django.contrib.auth.context_processors.auth',)
 
-
+GMAIL_CLIENT_ID = '864339645094-rii2aqr2jm20k63hf6jjt2pr7c6qku31.apps.googleusercontent.com'
+GMAIL_CLIENT_SECRET = 'nT3ebkSkWSUGdob-yfxNdCGC'
+GMAIL_SCOPE = 'https://www.googleapis.com/auth/gmail.readonly https://www.googleapis.com/auth/plus.me email'
+GMAIL_REDIRECT_URI = 'http://rdtcontactapp.herokuapp.com/oauth2callback'
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
